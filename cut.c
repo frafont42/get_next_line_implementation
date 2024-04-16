5e35495ddac7b219c3b53ef5e5d2f56b93602c2f
frafont42 <fra.fon11@gmail.com>
2024-04-16 19:53:50 +0200

    3rd

---
+++ cut.c
@@ -0,0 +1,24 @@
+char *cut(char *str)
+{
+	int i;
+	int j;
+	char *right_str;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\n')
+		i++;
+	right_str = (char *)malloc(sizeof(char) * i + 1);
+	while (str[j])
+	{
+		right_str[j] = str[j];
+		if (str[j] == '\n')
+		{
+			j++;
+			break;
+		}
+		j++;
+	}
+	right_str[j] = '\0';
+	return (right_str);
+}
